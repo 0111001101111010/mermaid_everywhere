d4045ebf629819997ca855cf593ff605fbfc8a3e
stanley zheng <dev.stanzheng@gmail.com>
2014-10-17 09:56:40 -0400

    adding a new map layer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // initialize the map
 var map = L.map('largemap').setView([36.888168,-76.266884], 12);
 L.tileLayer(
-  'http://{s}.sm.mapstack.stamen.com/($375a80[@60p],(naip,$2c235c[hsl-color@70])[darker],(toner-labels,$000[@20],$38315f[hsl-color])[@50])/{z}/{x}/{y}.png', {
+  'http://{s}.sm.mapstack.stamen.com/(toner,$fff[@60],$178282[hsl-color])/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href=”http://stamen.com”>Stamen Design</a>, under <a href=”http://creativecommons.org/licenses/by/3.0”>CC BY 3.0</a>. Data by <a href=”http://openstreetmap.org”>OpenStreetMap</a>, under <a href=”http://creativecommons.org/licenses/by-sa/3.0”>CC BY SA</a>',
     maxZoom: 18,
     minZoom: 6,
